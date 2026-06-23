753fc5671cbf5b47d808f2e824f87dc6f7cf60e6
Adrian Rascovschi <arascovschi@validis.com>
2026-06-23 15:17:00 +0300

    Added exercises

---
+++ src/main/java/StringsPractice.java
@@ -0,0 +1,15 @@
+
+public class StringsPractice {
+    public boolean isTheStringPalindrome(String str) {
+        return true;
+    }
+    public String makeArrayToString(char[] arr) {
+        return "";
+    }
+    public byte countVowels(String str){
+        return 1;
+    }
+    public byte countConsonants(String str){
+        return 1;
+    }
+}
---
+++ src/test/java/StringsPracticeTests.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringsPracticeTests {
+    private final StringsPractice stringsPractice = new StringsPractice();
+    @Test
+    public void isTheStringPalindromePositive(){
+     assertEquals(true, stringsPractice.isTheStringPalindrome("sdbnae"),"fveqwbvqebqebe");
+    }
+}
