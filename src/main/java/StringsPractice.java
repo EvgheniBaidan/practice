402cae79863691f7661ddbed8f772e4ee6b00226
EvgheniBaidan <baidanevghenii@gmail.com>
2026-06-26 13:13:07 +0300

    Resolved method isTheStringPalindrome,

--- src/main/java/StringsPractice.java
+++ src/main/java/StringsPractice.java
@@ -1,7 +1,14 @@
 
 public class StringsPractice {
     public boolean isTheStringPalindrome(String str) {
-        return true;
+        StringBuilder strBuild = new StringBuilder(str);
+        String reversedString = strBuild.reverse().toString();
+       /* if (str.equals(reversedString) && !str.isEmpty()) {
+        return true; }
+        else { return false;} solutia cu if else*/
+       // return str.equals(reversedString) && !str.isEmpty() ? true: false; solutia ternary operator
+        return str.equals(reversedString) && !str.isEmpty();// solutie operatiune logica
+
     }
     public String makeArrayToString(char[] arr) {
         return "";
--- src/test/java/StringsPracticeTests.java
+++ src/test/java/StringsPracticeTests.java
@@ -5,7 +5,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StringsPracticeTests {
     private final StringsPractice stringsPractice = new StringsPractice();
     @Test
-    public void isTheStringPalindromePositive(){
-     assertEquals(true, stringsPractice.isTheStringPalindrome("sdbnae"),"fveqwbvqebqebe");
+    public void isTheStringPalindromePositive(){ // positive test scenario
+     assertEquals(true, stringsPractice.isTheStringPalindrome("abba"),"Verify String is palindrome!");
+    }
+    @Test
+    public void checkIsStringNotPalindrome() {
+        assertEquals(false, stringsPractice.isTheStringPalindrome("arta"), "Verify the String is Not Palindrome");
+    }
+    @Test
+    public void checkEmptyStringIsNotPalindrome() {
+        assertEquals(false, stringsPractice.isTheStringPalindrome(""), "String is Empty, Not Palindrome!");
     }
 }
